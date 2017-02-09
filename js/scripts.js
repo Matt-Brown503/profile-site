908668b314b50814db98c9bfd3e0b9b245e512ce
Matt Brown <matt388@gmail.com>
2017-02-08 16:38:24 -0800

    responsive navbar added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log('Hello, Matt');
+
+/* navbar toggler */
+var openNav = function() {
+	var nav = document.getElementById('nav');
+	if (nav.className === "navbar") {
+		nav.className += " responsive";
+	}else { 
+		nav.className = "navbar";
+	}
+}
