8a1004856d7c505e93e40b7f737f812e8478cab7
Matt Brown <matt388@gmail.com>
2017-02-10 21:34:45 -0800

    navigate to section via navbar and close nav on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,20 @@
 console.log('Hello, Matt');
 
-/* navbar toggler */
-var openNav = function() {
-	var nav = document.getElementById('nav');
-	if (nav.className === "navbar") {
-		nav.className += " responsive";
-	}else { 
-		nav.className = "navbar";
-	}
-}
 
-document.getElementById('navOpener').addEventListener('click', function(event){
-	event.preventDefault();
-});
+$('#navOpener').on('click', function(){
+
+})
+
 
 $('a[href^="#"').on('click', function(event) {
+document.getElementById('sidebarToggle').checked = false;
 	var target = $(this.getAttribute('href'));
-
 	if(target.length) {
 		event.preventDefault();
 		$('html, body').stop().animate({
-			scrollTop: target.offset().top - $('#nav').height()
+			scrollTop: target.offset().top
 		}, 750);
 	}
+	
 });
+
